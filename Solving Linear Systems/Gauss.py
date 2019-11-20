35a4db387043e6b432199dc61d347afc70cb1e3a
bachbachkhoa <42577469+bachbachkhoa@users.noreply.github.com>
2019-11-20 18:30:27 +0700

    Update Gauss.py

--- Solving Linear Systems/Gauss.py
+++ Solving Linear Systems/Gauss.py
@@ -2,6 +2,11 @@
 from __future__ import unicode_literals
 import numpy as np
 
+'''Gauss Elimination to find exact root of linear system Ax = b
+   Input: matrix Ab such Ax = b
+   Output: vector x or message: 'The system has no unique root'
+'''
+
 def gauss(A):
 	'''Gauss Elimination to find exact root of linear system'''
 	m = len(A)
@@ -33,8 +38,27 @@ def main():
 	inFile = input("\nEnter input file name: ")
 	A = np.loadtxt(inFile)
 	A = A.tolist()
-	x = gauss(A)
-	print(x)
+
+	# Find rank of coefficient matrix and expanded coefficient matrix
+	n = len(A[0]) - 1	# number of variables in the system
+	B = []
+	for i in A:
+		B.append(i[:-1])
+	B = np.array(B)
+	A = np.array(A)
+	rA = np.linalg.matrix_rank(A)
+	rB = np.linalg.matrix_rank(B)
+
+	# Solve system
+	if rA == rB == n:
+		A = A.tolist()
+		x = gauss(A)
+		print("\nThe system has only a root:\n")
+		print(x)
+	elif rA == rB < n:
+		print("The system has infinite number of roots depending on", n - rA, "parameters")
+	else:
+		print("The system has no root")
 
 
 if __name__ == '__main__':
