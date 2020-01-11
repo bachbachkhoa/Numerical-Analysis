5d3c0f51e30ab0cd1441a9330b2f2a72b22a980c
bachbachkhoa <42577469+bachbachkhoa@users.noreply.github.com>
2020-01-11 17:09:07 +0700

    Update Picard.py

--- Approximate Solution of Normal Differential Equations/Picard.py
+++ Approximate Solution of Normal Differential Equations/Picard.py
@@ -1,1 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from sympy import *
+
+x, y = symbols('x y')
+
+
+def Picard(f, x0, y0, n):
+	'''f is symbolic, y(x0) = y0, n is number of iterations'''
+	k = 0
+	yBefore = y0
+	while True:
+		yAfter = y0 + integrate(f.subs(y, yBefore), (x, x0, x))
+		k += 1
+		if k >= n:
+			break
+		else:
+			yBefore = yAfter
+
+	return yAfter
