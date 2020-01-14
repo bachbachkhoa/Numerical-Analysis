ead06ad6e2f2030f5efdde55ea7b272a8a359f06
bachbachkhoa <42577469+bachbachkhoa@users.noreply.github.com>
2020-01-14 23:15:33 +0700

    Update GaussJordan.py

--- Find Inverse Matrix/GaussJordan.py
+++ Find Inverse Matrix/GaussJordan.py
@@ -1,1 +1,20 @@
+#Author: Duynt
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+import numpy as np
+
+def GaussJordan(A):
+	n = len(A)
+	I = np.eye(n)
+	x = np.arange(n)
+	for i in x:
+		pivot, u, v = A[i, i], np.copy(A[i, :]), np.copy(I[i, :])
+		A[i, :] /= pivot
+		I[i, :] /= pivot
+		y = np.delete(x, i)
+		for j in y:
+			z = A[j, i]
+			A[j, :] -= z*u/pivot
+			I[j, :] -= z*v/pivot
+	return I
